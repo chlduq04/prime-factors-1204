4b128ce40a65b139e9ffe8d6bccefc58cdc7653d
chlduq04 <chlduq04@gmail.com>
2024-04-26 17:09:02 +0900

    [feature] 30

--- src/main/java/PrimeFactor.java
+++ src/main/java/PrimeFactor.java
@@ -4,6 +4,9 @@ import java.util.List;
 public class PrimeFactor {
     public List<Integer> of(int i) {
         List<Integer> factors = new ArrayList<>();
+        if (i == 2) {
+            factors.add(2);
+        }
         return factors;
     }
 }
--- src/test/java/PrimeFactorsTest.java
+++ src/test/java/PrimeFactorsTest.java
@@ -5,9 +5,17 @@ import java.util.Arrays;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PrimeFactorsTest {
+
+
     @Test
-    public void test() {
+    public void testPrimefactorOf1() {
         PrimeFactor primeFactor = new PrimeFactor();
         assertEquals(Arrays.asList(), primeFactor.of(1));
+    }
+
+    @Test
+    public void testPrimefactorOf2() {
+        PrimeFactor primeFactor = new PrimeFactor();
+        assertEquals(Arrays.asList(2), primeFactor.of(2));
     }
 }
