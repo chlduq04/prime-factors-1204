6e8f4eb99319b4c3dd732b887d5cfd1f2d4de0d7
chlduq04 <chlduq04@gmail.com>
2024-04-26 16:52:35 +0900

    first commit

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,7 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+N
+src/main/java/PrimeFactor.java,e\d\edf3ea17aca95924c30283af95036f7669961286
+S
+#src/test/java/PrimeFactorsTest.java,f\b\fb148465151caa1ea43812b288a3ecf4c9025ae8
---
+++ src/test/java/PrimeFactorsTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrimeFactorsTest {
+    @Test
+    public void test() {
+        PrimeFactor primeFactor = new PrimeFactor();
+        assertEquals(10, 10);
+    }
+}
