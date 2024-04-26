8eec3c3796884d18a4d4d807daeaa4b0860a1fb9
chlduq04 <chlduq04@gmail.com>
2024-04-26 17:10:38 +0900

    [refactoring] 33

--- src/test/java/PrimeFactorsTest.java
+++ src/test/java/PrimeFactorsTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -6,16 +7,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PrimeFactorsTest {
 
+    PrimeFactor primeFactor;
+
+    @BeforeEach
+    void setUp() {
+        primeFactor = new PrimeFactor();
+    }
 
     @Test
     public void testPrimefactorOf1() {
-        PrimeFactor primeFactor = new PrimeFactor();
         assertEquals(Arrays.asList(), primeFactor.of(1));
     }
 
     @Test
     public void testPrimefactorOf2() {
-        PrimeFactor primeFactor = new PrimeFactor();
         assertEquals(Arrays.asList(2), primeFactor.of(2));
     }
 }
